68ee913c22e582bf81064036363c189b3d1a2b53
Gabriel Klein <gabriel.klein@revature.com>
2024-08-05 11:57:45 -0700

    Adding week 6, p2 data

---
+++ Week6/PySpark_Example.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("Example1").setMaster("local")
+
+sc = SparkContext(conf=conf)
+
+rdd = sc.parallelize([1, 2, 3, 4, 5])
+
+print(rdd.collect())
+
+pokemon_rdd = sc.textFile("/home/gabrielklein/pokemon.csv")
+
+pokemon_rdd_list = pokemon_rdd.map(lambda x: x.split(","))
+
+print(pokemon_rdd_list.take(5))
+
+pokemon_rdd_list.saveAsTextFile("/home/gabrielklein/pyspark_ex_out")
